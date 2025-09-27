611a82cb6f44cff359bbab47a34b83870bda4773
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-09-27 15:08:16 +0200

    Class 07

--- Class07(NotesApp)/Avenga.NotesApp/Avenga.NotesApp.Domain/Enums/Priority.cs
+++ Class07(NotesApp)/Avenga.NotesApp/Avenga.NotesApp.Domain/Enums/Priority.cs
@@ -1,6 +1,6 @@
 ﻿namespace Avenga.NotesApp.Domain.Enums
 {
-    // Enumeracija za priority na Note
+    // => Enumeracija za priority na Note
     // Brojkite ke se cuvaat vo bazata namesto tekst
     public enum Priority
     {
